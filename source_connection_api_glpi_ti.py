4f306a8a7c80ffa3873fa7dc776eb73ddf11f1b6
Richard Barbosa Silva <112967185+Richardbarbosasilva@users.noreply.github.com>
2025-08-14 11:04:08 -0400

    Update source_connection_api_glpi_ti.py

--- source_connection_api_glpi_ti.py
+++ source_connection_api_glpi_ti.py
@@ -5,14 +5,14 @@ import requests
 
 #session_token = fc23lteea86lq5ms8apvvh8sic
 
-url = "https://chamados.clickip.com.br/apirest.php/initSession"
+url = "http://instancia_glpi2/apirest.php/initSession"
 
 # Cabeçalhos necessários
 
 headers = {
     "Content-Type": "application/json", # ou "application/x-www-form-urlencoded" dependendo da API
-    "Authorization": "user_token ibUwLdHtUnGKGm3vTnfN05Fg2XQ3arhwNlEMyPyI", #"user_token SEU_USER_TOKEN"
-    "App-Token": "1bLyuIOpBIJrZIheMP0m9uqXgKD8rxoaxj8ruGlo"
+    "Authorization": "user_token <token>", #"user_token SEU_USER_TOKEN"
+    "App-Token": "<session_token>"
 }
 
 # Requisição GET para iniciar a sessão
@@ -22,7 +22,7 @@ response = requests.get(url, headers=headers)
 # Exibir resposta
 
 if response.status_code == 200:
-    print("Conexão com a API em glpi-ti OK")
+    print("Conexão com a API 2 OK")
     print(response.json())
 else:
     print(f"Erro {response.status_code}: {response.text}")
