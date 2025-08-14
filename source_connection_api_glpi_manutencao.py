af2133e999d994e1bb8a63704188546efe6b0c35
Richard Barbosa Silva <112967185+Richardbarbosasilva@users.noreply.github.com>
2025-08-14 11:03:27 -0400

    Update source_connection_api_glpi_manutencao.py

--- source_connection_api_glpi_manutencao.py
+++ source_connection_api_glpi_manutencao.py
@@ -4,14 +4,14 @@ import requests
 # Conexão de teste para verificar se a API está acessível em glpi-manutencao.clickip.local
 # session_token = nbu5tlkd6vold1ad5kv84kpfur
 
-url = "http://glpi-manutencao.clickip.local:5071/apirest.php/initSession"
+url = "http://instancia_glpi/apirest.php/initSession"
 
 # Cabeçalhos necessários
 
 headers = {
     "Content-Type": "application/json", # ou "application/x-www-form-urlencoded" dependendo da API
-    "Authorization": "user_token 5YImkRED6xBgKX7gg5qq6n4QTIj4y1bP549ETE1i", #"user_token SEU_USER_TOKEN"
-    "App-Token": "eYoExwDP3ghgK7VlM0e5XYiggsU987A97OtsC1KO"
+    "Authorization": "user_token <token>", #"user_token SEU_USER_TOKEN"
+    "App-Token": "<session_token>"
 }
 
 # Requisição GET para iniciar a sessão
@@ -21,7 +21,7 @@ response = requests.get(url, headers=headers)
 # Exibir resposta
 
 if response.status_code == 200:
-    print("Conexão com a API em glpi-manutencao OK")
+    print("Conexão com a API OK")
     print(response.json())
 else:
     print(f"Erro {response.status_code}: {response.text}")
